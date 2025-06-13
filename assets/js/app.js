7f50d52ea62735379582686cadae50498b03a6d3
Amal A M <vectorhero007@gmail.com>
2025-06-13 17:30:27 +0530

    new letter completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,23 @@ let slider = tns({
         1024: { items: 3 }
     }
 })
+
+
+let newsSlider = tns({
+      container: '.my-slider-news',
+      items: 1,
+      slideBy: 1,
+      autoplay: true,
+      autoplayButtonOutput: false,
+      controls: false,
+      nav: false,
+      gutter: 16,
+      loop: true,
+      speed: 500,
+      mouseDrag: true,
+      responsive: {
+        640: { items: 1 },
+        768: { items: 2 },
+        1024: { items: 3 }
+      }
+    });
